dce990f8c14c22ef6aed5b9b10affa14391a9f79
Florian Pircher <git@leuchtschirm.com>
2016-06-02 23:20:17 +0200

    Catch Exit Events

--- wwweb.js
+++ wwweb.js
@@ -9,3 +9,17 @@ class Protocol {
 let init = address => {
   
 }
+
+// Save on Exit
+
+process.stdin.resume()
+
+let quit = () => {
+  console.log('saving protocol...')
+  // TODO: save protocol
+  process.exit()
+}
+
+process.on('exit', quit)
+process.on('SIGINT', quit)
+process.on('uncaughtException', quit)
