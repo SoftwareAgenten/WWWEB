37ec890bda72a8f80aaac876ca70dab0bd6b1b38
Florian Pircher <git@leuchtschirm.com>
2016-06-03 16:04:08 +0200

    Remove Save on Quit Logic

--- wwweb.js
+++ wwweb.js
@@ -10,16 +10,4 @@ let init = address => {
   
 }
 
-// Save on Exit
 
-process.stdin.resume()
-
-let quit = () => {
-  console.log('saving protocol...')
-  // TODO: save protocol
-  process.exit()
-}
-
-process.on('exit', quit)
-process.on('SIGINT', quit)
-process.on('uncaughtException', quit)
