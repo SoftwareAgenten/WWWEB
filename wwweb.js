3bc220704277e77299eaeabb529cd463f947755e
Florian Pircher <git@leuchtschirm.com>
2016-06-03 16:33:10 +0200

    Add Startup Logic With Domain Prompt

--- wwweb.js
+++ wwweb.js
@@ -1,13 +1,46 @@
 'use strict'
 
+// Node Modules
+
+const readline = require('readline')
+
+// Modules Setup
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+// Classes
+
 class Protocol {
+  constructor() {}
+}
+
+class Report {
   constructor(initialAddress) {
     this.initialAddress = initialAddress
+    this.protocol = new Protocol()
   }
 }
 
-let init = address => {
+// Main Routine
+
+const WWWEB = (() => {
+  const self = {
+    report: null
+  }
   
-}
+  self.init = initialAddress => {
+    self.report = new Report(initialAddress)
+  }
+  
+  return self
+}())
 
+// Startup Logic
 
+rl.question('Initial domain: ', answer => {
+  WWWEB.init(answer)
+  rl.close()
+})
